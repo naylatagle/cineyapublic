8b7b1e19fe136c5ecae77977f82242d1a924f5ee
Juan Dallaglio <jdallaglio@wondersoft.com.ar>
2020-06-13 16:42:20 -0300

    Correccion

---
+++ app/src/main/java/com/example/alumno/cineya/Constants.java
@@ -0,0 +1,8 @@
+package com.example.alumno.cineya;
+
+public class Constants {
+    public final static String SHARED_KEY_LOGIN_TYPE = "tipoDeLogin";
+
+    public final static int LOGIN_TYPE_NONE = 0;
+    public final static int LOGIN_TYPE_MAIL = 3;
+}
